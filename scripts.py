ca5fb12576456a8b0bc4ee390a22df13ae2bcdd3
swe-jaeyoungpark <swe.jaeyoungpark@navercorp.com>
2019-08-05 13:32:54 +0900

    Add exception handling

--- scripts.py
+++ scripts.py
@@ -1,19 +1,25 @@
 #-*- coding: utf-8 -*-
 import atexit
 
+from selenium.common.exceptions import WebDriverException
+
 from src import consts
 from src.crawler import search_board, open_browser
 from src.signin import ensure_signin
-
+from src.utils import console
 
 if __name__ == '__main__':
-    driver = open_browser(consts.DRIVER_PATH)
-    atexit.register(lambda: driver.quit())
+    while True:
+        try:
+            driver = open_browser(consts.DRIVER_PATH)
+            atexit.register(lambda: driver.quit())
 
-    ensure_signin(driver)
+            ensure_signin(driver)
 
-    ## Search author of <site_url>/bbs/view.php?id=mento&no=1423661
-    # search_board(driver, "mento", "1423661")
+            ## Search author of <site_url>/bbs/view.php?id=mento&no=1423661
+            # search_board(driver, "mento", "1423661")
 
-    ## Search author of <site_url>/bbs/view.php?id=ob&no=167039, in latest 100 articles.
-    search_board(driver, "ob", "167039", 100)
+            ## Search author of <site_url>/bbs/view.php?id=ob&no=167039, in latest 100 articles.
+            search_board(driver, "ob", "167039", 100)
+        except WebDriverException as e:
+            console.error(e)
